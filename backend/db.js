d82679a00f18bfb5caeaff3fdfc72ae71b5c5781
tyurg <dimonballon@gmail.com>
2026-06-02 14:00:19 +0500

    сколько можно уже

--- backend/db.js
+++ backend/db.js
@@ -5,11 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 pool.on('error', (err) => {
